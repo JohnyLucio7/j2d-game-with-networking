c44983e547abe9d9186baa8fa7e6c75bc2906d51
lucio <lucio@pop-os.localdomain>
2022-05-05 15:01:19 -0300

    Setting up the JFrame

--- j2d-game-with-networking/src/game/Game.java
+++ j2d-game-with-networking/src/game/Game.java
@@ -1,15 +1,127 @@
 package game;
 
+import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
-public class Game extends Canvas implements Runnable{
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 160;
+	public static final int HEIGHT = 120;
+	public static final int SCALE = 3;
+	public static final String NAME = "Game";
+
+	public boolean running = false;
+	private JFrame frame;
+	private Thread thread;
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+	private int ticksCount;
+
+	public Game() {
+		initFrame();
+	}
+
+	private void initFrame() {
+		setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+
+		frame = new JFrame(NAME);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.add(this, BorderLayout.CENTER);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+
+	}
+
+	public synchronized void stop() {
+		running = false;
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		long lastTime = System.nanoTime();
+		double nsPerTick = 1000000000 / 60;
+		int ticks = 0;
+		int frames = 0;
+		long lastTimer = System.currentTimeMillis();
+		double delta = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / nsPerTick;
+			lastTime = now;
+
+			if (delta >= 1) {
+				ticks++;
+				frames++;
+				delta -= 1;
+				tick();
+				render();
+			}
+
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			if (System.currentTimeMillis() - lastTimer >= 1000) {
+				lastTimer += 1000;
+				System.out.println("Frames: " + frames + " , " + "Ticks: " + ticks);
+				frames = 0;
+				ticks = 0;
+			}
+
+		}
+
+	}
+
+	public void tick() {
+		ticksCount++;
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = i + ticksCount;
+		}
+	}
+
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+
+		Graphics g = bs.getDrawGraphics();
+
+		// g.setColor(new Color(40, 40, 40));
+		// g.fillRect(0, 0, getWidth(), getHeight());
+
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+
+		g.dispose();
+		bs.show();
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
 	}
 
 }
