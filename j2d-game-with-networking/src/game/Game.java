004472888006282152295139a0c5e1bb527fc6d6
Johny Lúcio <59095834+JohnyLucio7@users.noreply.github.com>
2022-05-05 11:26:04 -0300

    iniciando projeto

---
+++ j2d-game-with-networking/src/game/Game.java
@@ -0,0 +1,15 @@
+package game;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
